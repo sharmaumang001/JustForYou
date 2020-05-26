af0521ee2ae6e37bab17d905a0bc02c6ff8face2
umang <iumangsharma001@gmail.com>
2020-05-26 14:38:39 +0530

    First activity complete.second added.

--- app/src/main/java/com/example/justforyou/MainActivity.java
+++ app/src/main/java/com/example/justforyou/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.justforyou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mNextView;
+        mNextView= findViewById(R.id.button);
+
+        mNextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(MainActivity.this,Main2Activity.class);
+                finish();
+                startActivity(mIntent);
+            }
+        });
+
+
     }
 }
