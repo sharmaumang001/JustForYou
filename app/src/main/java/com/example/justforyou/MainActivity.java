8cb964e343b4003752846bf7b10bfe3d2d95aab1
umang <iumangsharma001@gmail.com>
2020-06-07 20:11:14 +0530

    welcome screen animation added.

--- app/src/main/java/com/example/justforyou/MainActivity.java
+++ app/src/main/java/com/example/justforyou/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.justforyou;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mNextView;
+    ConstraintLayout mlayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mNextView;
+
         mNextView= findViewById(R.id.button);
+        mlayout= findViewById(R.id.MainAcitvity_ID);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable)mlayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(1000);
+        animationDrawable.start();
 
         mNextView.setOnClickListener(new View.OnClickListener() {
             @Override
